9fcdf057d5844fe7fba288ffa182ef48e20de9cb
Matheus Alves <159180480+Maziin07@users.noreply.github.com>
2025-02-18 15:55:57 -0300

    Add files via upload

---
+++ lista-variaveis/script.js
@@ -0,0 +1,61 @@
+
+// 1 // 
+
+var nome = "Matheus";
+var idade = 17;
+var cidade = "Sorocaba"
+
+console.log(" Meu nome é ", nome, ", eu tenho ", idade, "anos e moro em ", cidade);7
+
+// 2 //
+
+var nome;
+var _idade;
+var cidadenatal;
+var $preco;
+var let;
+
+// 3 //
+
+var altura = "";
+console.log(altura); // Resposta: não acontece nada :D //
+
+// 4 //
+
+var altura = "10";
+console.log(altura);
+
+// 5 //
+
+"use strict";
+idade = 10;
+console.log(idade); // Resposta: da erro e fala que a variavel não foi definida //
+
+// 6 //
+
+var contador = 0;
+
+contador += 1;
+contador += 1;
+contador += 1;
+
+console.log(contador);
+
+// 7 //
+
+const PI = 3.14;
+PI = 3.14159;
+console.log(PI); // Resposta: ele da erro //
+
+// 8 //
+
+// var: se declarada dentro de uma função, ela é acessível em toda a função. Se declarada fora de uma função, ela é acessível globalmente. //
+
+// let: é acessível apenas dentro do bloco onde foi declarada. //
+
+// const: o valor de const não pode ser reatribuido. //
+
+{
+    let = 13;
+}
+
