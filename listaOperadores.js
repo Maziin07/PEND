f27d442fd964f00cbb3ce12122ee2d8a6e2e0440
Matheus Alves <159180480+Maziin07@users.noreply.github.com>
2025-03-25 15:57:03 -0300

    Add files via upload

---
+++ listaOperadores.js
@@ -0,0 +1,112 @@
+// 1
+
+console.log("Exercicio 1: ")
+
+let x = 10;
+console.log(x); 
+
+console.log("  ");
+
+// 2
+
+console.log("Exercicio 2: ")
+
+let a = 20;
+let b = 5;
+
+soma = a + b;
+subtracao = a - b;
+multiplicacao = a * b;
+divisao = a / b;
+restodivisao = a % b;
+
+console.log(soma, subtracao, multiplicacao, divisao, restodivisao);
+
+console.log("  ");
+
+// 3
+
+console.log("Exercicio 3: ")
+
+let num = -15;
+console.log(-num);
+
+console.log("  ");
+
+// 4
+
+console.log("Exercicio 4: ")
+
+let count = 5;
+console.log(++count);
+console.log(--count);
+
+console.log("  ");
+
+// 5
+
+console.log("Exercicio 5: ")
+
+{ 
+    let x = 10;
+    console.log(x += 5);
+    console.log(x -= 3);
+    console.log(x *= 2);
+    console.log(x /= 4);
+    console.log(x %= 3);
+}
+
+console.log("  ");
+
+// 6
+
+console.log("Exercicio 6: ")
+
+let p = true;
+let q = false;
+
+console.log(p && q);
+console.log(p || q);
+console.log(!p);
+console.log(!q);
+
+console.log("  ");
+
+// 7
+
+console.log("Exercicio 7: ")
+
+console.log("exe7");
+console.log(0 || "Hello");          // Saída: "Hello"
+console.log("" && "World");          // Saída: ""
+console.log(null ?? "Default Value"); // Saída: "Default Value"
+console.log(undefined ?? 42);          // Saída: 42
+
+console.log("  ");
+
+// 8
+
+console.log("Exercicio 8: ")
+
+let resultado = 10 + 5 * 2 > 20 && !false;
+console.log(resultado); 
+
+// false
+
+console.log("  ");
+
+// 9
+
+console.log("Exercicio 9: ")
+
+
+
+console.log("  ");
+
+// 10
+
+console.log("Exercicio 10: ")
+
+
+
+console.log("  ");
